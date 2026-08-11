be7aa5aa53a167219fa098a1fd2444c62fac9892
Rostislav <promotion.work.pal@gmail.com>
2026-08-11 20:20:37 +1000

    Update index.ts

--- form/text/index.ts
+++ form/text/index.ts
@@ -2,3 +2,4 @@ export { PromFieldInput } from './prom.form.text';
 export * from './prom.form.text';
 export * from './prom.form.pattern';
 export * from './prom.form.text.stories';
+export * from './prom.form.mask';
